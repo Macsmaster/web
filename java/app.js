c6706d1122c2d85cbf9a203aa5ae5bd28a5672a1
Miguel Coy <miguelcoytriana@gmail.com>
2019-07-14 08:47:22 -0500

    firts

---
+++ java/app.js
@@ -0,0 +1,12 @@
+
+
+var btnMenu = document.getElementById("icon-menu");
+var nav = document.getElementById("nav");
+
+btnMenu.addEventListener('click', function(){
+nav.classList.toggle('mostrar');
+})
+
+
+
+
